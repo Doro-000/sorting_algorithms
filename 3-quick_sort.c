a5da6c12658ab7ff53301528f985ca9e176a8cc9
Phili <Philimongames@gmail.com>
2021-06-15 16:55:08 +0300

    added 3-qucik sorted

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,49 +1,74 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "sort.h"
 
+/**
+ * quick_sort - quick sort algorithm
+ * @array: array to be sorted
+ * @size: size of array
+ *
+ * Return: null
+ */
 void quick_sort(int *array, size_t size)
 {
-	int lo = 0;
-	int hi = size - 1;
-	int p;
-
-	if (lo < hi)
-	{
-		p = partition(array, size);
-		print_array(array, size);
-		if (p != -1)
-		{
-			quick_sort(array, p);
-			quick_sort(array + (p + 1), size - (p + 1));
-		}
-	}
+    if (size >= 2)
+    {
+        back(array, 0, size - 1, size);
+    }
 }
-// {16, 8, 19, 2, 4}
-//don touch
-int partition(int *array, size_t size)
-{
-	size_t i, j;
-	int pivot, temp;
 
-	if (size < 2)
-		return (-1);
-	pivot = array[size - 1];
-	i = 0;
-	j = size - 1;
-	while (1)
-	{
-		while (array[i] < pivot && i < size)
-			i++;
-		while (array[j] >= pivot && j)
-			j--;
-		if (j <= i)
-			break;
-		temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
-	array[size - 1] = array[i];
-	array[i] = pivot;
-	return (i);
+/**
+ * back - sort via quick sort
+ * @array: array to be sorted
+ * @a: higher index
+ * @b: lower index
+ * @size: size of array
+ *
+ * Return: void function
+ */
+void back(int *array, int a, int b, size_t size)
+{
+    int part;
+
+    if (a < b)
+    {
+        part = partition(array, a, b, size);
+        // printf("partition = %d\n", part);
+        if (part != 0)
+            back(array, a, part - 1, size);
+        // printf("second\n");
+        back(array, part + 1, b, size);
+    }
+}
+
+/**
+ * partition - partition point
+ * @array: array to be partitioned
+ * @hi: highers index
+ * @low: lowest index
+ * @size: size of array
+ *
+ * Return: partition point
+ */
+int partition(int *array, int low, int hi, size_t size)
+{
+    int j = low;
+    size_t i;
+
+    for (i = j; i < size; i++)
+    {
+        if (array[i] < array[hi])
+        {
+            if (i != j)
+            {
+                swaper(i, j, array);
+                print_array(array, size);
+            }
+            j++;
+        }
+    }
+    if (j != hi)
+    {
+        swaper(j, hi, array);
+        print_array(array, size);
+    }
+    return j;
 }
