73f77453f74e7329492f5bce8213a2d5e01f5d26
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-16 16:23:31 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,32 +1,65 @@
-#include <stdlib.h>
 #include "sort.h"
 
 /**
- * shell_sort - implementation of shell_sort
- * @array: array to be sorted
- * @size: size of the array
+ * cocktail_sort_list - cocktail sorting method
+ * @list: head of linked list
+ *
+ * Return: void function
+ */
+
+void cocktail_sort_list(listint_t **list)
+{
+    listint_t *temp = *list;
+    int swapped;
+
+    do
+    {
+        swapped = 0;
+        while (temp->next != NULL)
+        {
+            if ((temp->n) > ((temp->next)->n))
+            {
+                swap_nodes(temp, temp->next);
+                print_list(*list);
+                swapped = 1;
+                continue;
+            }
+            temp = temp->next;
+        }
+        if (swapped == 0)
+            break;
+        swapped = 0;
+        while (temp->prev != NULL)
+        {
+            if ((temp->n) < ((temp->prev)->n))
+            {
+                swap_nodes(temp->prev, temp);
+                swapped = 1;
+                if (temp->prev == NULL)
+                    *list = temp;
+                print_list(*list);
+                continue;
+            }
+            temp = temp->prev;
+        }
+    } while (swapped);
+}
+
+/**
+ * swap_nodes - swap adjacent nodes of a doubly linked list
+ * @first: first node
+ * @second: second node
  *
  * Return: void
  */
-void shell_sort(int *array, size_t size)
+void swap_nodes(listint_t *first, listint_t *second)
 {
-	size_t interval = 1, i, j;
-	int temp;
-
-	while (interval < size)
-		interval = interval * 3 + 1;
-
-	while (interval)
-	{
-		for (i = interval; i < size; i += 1)
-		{
-			temp = array[i];
-			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
-				array[j] = array[j - interval];
-			array[j] = temp;
-		}
-		if (interval < size)
-			print_array(array, size);
-		interval /= 3;
-	}
+    if (first->prev != NULL)
+        (first->prev)->next = second;
+    if (second->next != NULL)
+        (second->next)->prev = first;
+    second->prev = first->prev;
+    first->prev = second;
+    first->next = second->next;
+    second->next = first;
 }
