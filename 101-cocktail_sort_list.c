781a20566ba9e222fc13c2eea4a6ff837258501f
philimon-reset <58902914+philimon-reset@users.noreply.github.com>
2021-06-16 16:28:49 +0300

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -9,40 +9,40 @@
 
 void cocktail_sort_list(listint_t **list)
 {
-    listint_t *temp = *list;
-    int swapped;
+	listint_t *temp = *list;
+	int swapped;
 
-    do
-    {
-        swapped = 0;
-        while (temp->next != NULL)
-        {
-            if ((temp->n) > ((temp->next)->n))
-            {
-                swap_nodes(temp, temp->next);
-                print_list(*list);
-                swapped = 1;
-                continue;
-            }
-            temp = temp->next;
-        }
-        if (swapped == 0)
-            break;
-        swapped = 0;
-        while (temp->prev != NULL)
-        {
-            if ((temp->n) < ((temp->prev)->n))
-            {
-                swap_nodes(temp->prev, temp);
-                swapped = 1;
-                if (temp->prev == NULL)
-                    *list = temp;
-                print_list(*list);
-                continue;
-            }
-            temp = temp->prev;
-        }
-    } while (swapped);
+	do
+	{
+		swapped = 0;
+		while (temp->next != NULL)
+		{
+			if ((temp->n) > ((temp->next)->n))
+			{
+				swap_nodes(temp, temp->next);
+				print_list(*list);
+				swapped = 1;
+				continue;
+			}
+			temp = temp->next;
+		}
+		if (swapped == 0)
+			break;
+		swapped = 0;
+		while (temp->prev != NULL)
+		{
+			if ((temp->n) < ((temp->prev)->n))
+			{
+				swap_nodes(temp->prev, temp);
+				swapped = 1;
+				if (temp->prev == NULL)
+					*list = temp;
+				print_list(*list);
+				continue;
+			}
+			temp = temp->prev;
+		}
+	} while (swapped);
 }
 
 /**
@@ -54,12 +54,12 @@ void cocktail_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t *first, listint_t *second)
 {
-    if (first->prev != NULL)
-        (first->prev)->next = second;
-    if (second->next != NULL)
-        (second->next)->prev = first;
-    second->prev = first->prev;
-    first->prev = second;
-    first->next = second->next;
-    second->next = first;
+	if (first->prev != NULL)
+		(first->prev)->next = second;
+	if (second->next != NULL)
+		(second->next)->prev = first;
+	second->prev = first->prev;
+	first->prev = second;
+	first->next = second->next;
+	second->next = first;
 }
